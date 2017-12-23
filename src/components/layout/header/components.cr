9cf1ebec3e8f92b4d969987fb785fdb1edc333f5
mikeeus <itsmikias@gmail.com>
2017-12-24 01:02:50 +0300

    Move blog link to development

--- src/components/layout/header/components.cr
+++ src/components/layout/header/components.cr
@@ -3,12 +3,12 @@ module Layout::Header::Components
     div class: "nav" do
       link brand.to_s, class: "brand", to: "/"
       ul role: "navigation" do
-        links.each do |label, url|
-          li do
-            link label.to_s, to: url, class: "m-button"
-          end
-        end
         if Lucky::Env.development?
+          links.each do |label, url|
+            li do
+              link label.to_s, to: url, class: "m-button"
+            end
+          end
           li do
             link "+ post", to: "/blog/posts/new", class: "m-button"
           end
