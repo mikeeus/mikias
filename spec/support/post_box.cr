40c6711faffedaf43e81bcf40ccb554742ef1a51
mikeeus <itsmikias@gmail.com>
2017-12-24 10:46:42 +0300

    Add commented out PostBox

---
+++ spec/support/post_box.cr
@@ -0,0 +1,15 @@
+# class PostBox < BaseBox
+#   def initialize
+#     title "Test post"
+#     content "This is the introductory paragraph.
+
+#     First body paragraph.
+
+#     Second body paragraph.
+
+#     Conclusion paragraph."
+#     tags "test, tag"
+#     published_at Time.now
+#     comment_id 1
+#   end
+# end
