9e146d64faa28be2901f43f71e16ccf33f5e1f96
mikeeus <itsmikias@gmail.com>
2017-12-24 11:17:41 +0300

    Allow changing published_at date

--- spec/support/factories.cr
+++ spec/support/factories.cr
@@ -1,4 +1,4 @@
-def insert_post(title = "title", content = "content", published_at = Time.now - 1.day, tags : String? = nil)
+def insert_post(title = "title", content = "content", published_at = Time.now - 1.day, tags : String? = "some,tags")
   LuckyRecord::Repo.run do |db|
     db.exec "INSERT into posts(created_at, updated_at, title, content, tags, published_at, comment_id) VALUES ($1, $2, $3, $4, $5, $6, $7)",
       Time.now,
