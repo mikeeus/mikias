94b299dfcd2203a7752819e0ede21bc0e60daf8f
mikeeus <itsmikias@gmail.com>
2017-12-29 09:25:09 +0300

    Clean up, add success flash

--- src/actions/posts/create.cr
+++ src/actions/posts/create.cr
@@ -1,14 +1,10 @@
 class Posts::Create < AdminAction
-  post "/posts/create" do
-    puts "hello"
+  action do
     PostForm.create(params) do |form, post|
-      pp form
-      pp post
       if post
+        flash.success = "Post successfully created!"
         redirect Posts::Index
       else
-        @context.flash.now[:just_for_this_request] = "error: #{pp form.errors}"
-        pp form.errors
         render Posts::NewPage, post_form: form
       end
     end
